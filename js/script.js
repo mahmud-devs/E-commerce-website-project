67ea6dc45643938a01c3bebb1e493c6f9f7d6187
mahmud-devs <mahmudulislam0999@gmail.com>
2023-08-25 01:15:05 +0600

    best seller part

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 $(function(){
 
-    // --------slick slider---------
+    // --------banner slick slider---------
     $('.slick__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        // centerMode: true,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              autoplay: false,
+              autoplaySpeed: 2500,
+            }
+          }
+        ]
+    });
+    
+    // --------big content slick slider---------
+    $('.big__slider').slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
